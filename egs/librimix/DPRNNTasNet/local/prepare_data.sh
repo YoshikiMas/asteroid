a7e1311b0dbe39bc9cd52bba0fa6c05588c9ff86
Yoshiki <mas03151997@gmail.com>
2023-03-16 18:09:09 +0900

    bug fix in data preparation

--- egs/librimix/DPRNNTasNet/local/prepare_data.sh
+++ egs/librimix/DPRNNTasNet/local/prepare_data.sh
@@ -10,9 +10,8 @@ if [[ $n_src -le  1 ]]
 then
   changed_n_src=2
 else
-  changed_n_src=n_src
+  changed_n_src=$n_src
 fi
-
 $python_path local/create_local_metadata.py --librimix_dir $storage_dir/Libri$changed_n_src"Mix"
 
 $python_path local/get_text.py \
