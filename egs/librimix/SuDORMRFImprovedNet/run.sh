11ca5bfd06f9b71cd4cb7987fbac2691e33f8ccc
Yoshiki <mas03151997@gmail.com>
2023-03-16 20:33:16 +0900

    modify enh recipe on LibriMix

--- egs/librimix/SuDORMRFImprovedNet/run.sh
+++ egs/librimix/SuDORMRFImprovedNet/run.sh
@@ -7,41 +7,31 @@ set -o pipefail
 # If you haven't generated LibriMix start from stage 0
 # Main storage directory. You'll need disk space to store LibriSpeech, WHAM noises
 # and LibriMix. This is about 500 Gb
-storage_dir=
+storage_dir=/home/ymasuyama/LibriMix/data
 
 # After running the recipe a first time, you can run it from stage 3 directly to train new models.
 
 # Path to the python you'll use for the experiment. Defaults to the current python
 # You can run ./utils/prepare_python_env.sh to create a suitable python environment, paste the output here.
-python_path=python
+python_path=/opt/miniconda3/bin/python
 
 # Example usage
 # ./run.sh --stage 3 --tag my_tag --task sep_noisy --id 0,1
 
 # General
 stage=0  # Controls from which stage to start
-tag=""  # Controls the directory name associated to the experiment
+stop_stage=10000
+tag="asteroid_default"  # Controls the directory name associated to the experiment
 # You can ask for several GPUs using id (passed to CUDA_VISIBLE_DEVICES)
-id=0,1,2,3
+id=$CUDA_VISIBLE_DEVICES
 out_dir=librimix # Controls the directory name associated to the evaluation results inside the experiment directory
 
-# Network config
-# Training config
-epochs=200
-batch_size=2
-num_workers=4
-half_lr=yes
-early_stop=yes
-# Optim config
-optimizer=adam
-lr=0.001
-weight_decay=0.
 # Data config
 sample_rate=16000
+task=enh_single  # one of 'enh_single', 'enh_both', 'sep_clean', 'sep_noisy'
 mode=max
 n_src=1
-segment=3
-task=enh_single  # one of 'enh_single', 'enh_both', 'sep_clean', 'sep_noisy'
+segment=2.5
 
 eval_use_gpu=1
 # Need to --compute_wer 1 --eval_mode max to be sure the user knows all the metrics
@@ -54,6 +44,8 @@ eval_mode=
 
 sr_string=$(($sample_rate/1000))
 suffix=wav${sr_string}k/$mode
+dumpdir=data/$suffix  # directory to put generated json file
+[[ ! -d $dumpdir ]] && mkdir -p $dumpdir
 
 if [ -z "$eval_mode" ]; then
   eval_mode=$mode
@@ -63,8 +55,8 @@ train_dir=data/$suffix/train-360
 valid_dir=data/$suffix/dev
 test_dir=data/wav${sr_string}k/$eval_mode/test
 
-if [[ $stage -le  0 ]]; then
-	echo "Stage 0: Generating Librimix dataset"
+if [ $stage -le  1 ] && [ ${stop_stage} -ge 1 ]; then
+	echo "Stage 1: Generating Librimix dataset"
 	if [ -z "$storage_dir" ]; then
 		echo "Need to fill in the storage_dir variable in run.sh to run stage 0. Exiting"
 		exit 1
@@ -72,8 +64,8 @@ if [[ $stage -le  0 ]]; then
   . local/generate_librimix.sh --storage_dir $storage_dir --n_src $n_src
 fi
 
-if [[ $stage -le  1 ]]; then
-	echo "Stage 1: Generating csv files including wav path and duration"
+if [ $stage -le  2 ] && [ ${stop_stage} -ge 2 ]; then
+	echo "Stage 2: Generating csv files including wav path and duration"
   . local/prepare_data.sh --storage_dir $storage_dir --n_src $n_src
 fi
 
@@ -88,24 +80,16 @@ mkdir -p $expdir && echo $uuid >> $expdir/run_uuid.txt
 echo "Results from the following experiment will be stored in $expdir"
 
 
-if [[ $stage -le 2 ]]; then
-  echo "Stage 2: Training"
+if [ $stage -le  3 ] && [ ${stop_stage} -ge 3 ]; then
+  echo "Stage 3: Training"
   mkdir -p logs
-  CUDA_VISIBLE_DEVICES=$id $python_path train.py --exp_dir $expdir \
-		--epochs $epochs \
-		--batch_size $batch_size \
-		--num_workers $num_workers \
-		--half_lr $half_lr \
-		--early_stop $early_stop \
-		--optimizer $optimizer \
-		--lr $lr \
-		--weight_decay $weight_decay \
+  CUDA_VISIBLE_DEVICES=$id $python_path train.py \
 		--train_dir $train_dir \
 		--valid_dir $valid_dir \
 		--sample_rate $sample_rate \
 		--n_src $n_src \
 		--task $task \
-		--segment $segment | tee logs/train_${tag}.log
+		--exp_dir $expdir | tee logs/train_${tag}.log
 	cp logs/train_${tag}.log $expdir/train.log
 
 	# Get ready to publish
@@ -114,8 +98,8 @@ if [[ $stage -le 2 ]]; then
 fi
 
 
-if [[ $stage -le 2 ]]; then
-	echo "Stage 2 : Evaluation"
+if [ $stage -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+	echo "Stage 4 : Evaluation"
 
 	if [[ $compute_wer -eq 1 ]]; then
 	  if [[ $eval_mode != "max" ]]; then
